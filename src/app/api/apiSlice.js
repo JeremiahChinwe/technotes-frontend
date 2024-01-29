1201974564a1c1ce3e9116b4e41a24041091c378
Jeremiah Chinwe <chinwejeremiah9@gmail.com>
2024-01-29 11:22:53 +0100

    Updated the baseUrl property

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -3,7 +3,7 @@ import { setCredentials } from "../../features/auth/authSlice"
 
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: "https://technotes-restapi.vercel.app",
+    baseUrl: "https://technotes-restapi.onrender.com",
     credentials: "include",
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
